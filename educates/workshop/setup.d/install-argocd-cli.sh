55ead2b3771dfb61749696912359510d26245853
Daniel Bodky <dbodky@gmail.com>
2024-04-10 08:51:06 +0200

    Update setup scripts

---
+++ educates/workshop/setup.d/install-argocd-cli.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+# create user-local bin directory; it's already part of $PATH
+mkdir -p /home/eduk8s/bin
+
+# get CLI from GitHub release and install it to /home/eduk8s/bin
+curl -sLo /home/eduk8s/bin/argocd https://github.com/argoproj/argo-cd/releases/download/v2.10.6/argocd-linux-amd64
+chmod 755 /home/eduk8s/bin/argocd
